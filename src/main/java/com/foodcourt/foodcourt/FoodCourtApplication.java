97617759020431d45eaa71bfb005c77a678046e7
neiderb <neider.bustos@pragma.com.co>
2025-11-04 16:24:44 -0500

    First commit

---
+++ src/main/java/com/foodcourt/foodcourt/FoodCourtApplication.java
@@ -0,0 +1,16 @@
+package com.foodcourt.foodcourt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCourtApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(
+			FoodCourtApplication.class,
+			args
+		);
+	}
+	
+}
---
+++ src/test/java/com/foodcourt/foodcourt/FoodCourtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodcourt.foodcourt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCourtApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
